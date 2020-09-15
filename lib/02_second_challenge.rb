7b657bb732cf00bd9c4467686b04325115731d28
Raynaldo Sutisna <raaynaldo@hotmail.com>
2020-09-15 21:52:25 +0000

    Automatically backed up by Learn

--- lib/02_second_challenge.rb
+++ lib/02_second_challenge.rb
@@ -5,13 +5,13 @@ def second_challenge
       :patriarch => {},
       :matriarch => {},
       :hero => {},
-      :hero_friends => {}
+      :hero_friends => []
       },
     :capulet => {
       #your key/value pairs here!
       :patriarch => {},
       :matriarch => {},
       :heroine => {},
-      :heroine_friends => {}
+      :heroine_friends => []
       }}
 end
